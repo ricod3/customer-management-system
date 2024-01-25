17e23fb051efaf92fc443ba1f777c816f4f124be
Rijad <113040547+ricod3@users.noreply.github.com>
2024-01-25 15:17:50 +0100

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,9 +1,9 @@
 <?php
 
-$servername = "mysql-3caa43ed-rico-88c5.a.aivencloud.com:22995";
-$username = "avnadmin";
-$password = "AVNS_EwJ2Wr9Qt13jJTX0x_9";
-$dbname = "cms-cloud-mysql-db";
+$servername = "mysql-";
+$username = "";
+$password = "";
+$dbname = "";
 
 
 try {
